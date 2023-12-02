1f0973e487a4eefb4851e8d5adf6ec13cbcaaaad
unknown <mdamir4298@gmail.com>
2023-12-02 22:10:40 +0530

    search functionality done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,29 +2,63 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import Card from "./Components/Card";
 
+// Main App Component
 function App() {
+  // State for storing fetched data, loading status, and search term
   const [datas, setDatas] = useState([]);
-  
+  const [loading, setLoading] = useState(false);
+  const [search, setSearch] = useState("");
 
+  // Function to construct the API URL with the search query
+  const getData = (url, search) => {
+    if (search) {
+      url = `${url}?beer_name=${search}`;
+    }
+    return url;
+  };
+
+  // Function to fetch data from the API
   const fetchData = async () => {
+    let api = getData(`https://api.punkapi.com/v2/beers`, search);
     try {
-      let response = await fetch("https://api.punkapi.com/v2/beers");
+      setLoading(true);
+      let response = await fetch(api);
       let data = await response.json();
+      // Log the fetched data to the console
       console.log(data);
       setDatas(data);
+      setLoading(false);
     } catch (err) {
-      console.log(err);
+      // Log any errors that occur during the fetch operation
+      console.log("Error while fetching data:", err);
     }
   };
 
   console.log(datas);
 
+  // Effect hook to fetch data when the component mounts or when the search term changes
   useEffect(() => {
     fetchData();
-  }, []);
+  }, [search]);
+
+  // JSX structure for the App component
   return (
     <div className="App">
-      <Card datas={datas} />
+      <h1>Cards data with images</h1>
+      <div>
+        <label htmlFor="">Search:</label>
+        <br />
+        <input
+          type="text"
+          placeholder="search by name"
+          value={search}
+          onChange={(e) => {
+            setSearch(e.target.value);
+            console.log(e.target.value);
+          }}
+        />
+      </div>
+      <Card datas={datas} loading={loading} />
     </div>
   );
 }
--- my-app/src/Components/Card.jsx
+++ my-app/src/Components/Card.jsx
@@ -1,24 +1,47 @@
-import React, {  useState } from "react";
-
-const Card = ({datas}) => {
-    
+// Card Component
+const Card = ({ datas, loading }) => {
+  // Log loading status and datas to the console
+  console.log(loading, datas);
 
   return (
-    <div style={{margin:"5px",marginTop:"15px"}}>
-      <h1>Cards data with images</h1>
-      <div style={{display:"grid",gridTemplateColumns:"repeat(4,1fr)",gap:"5px"}}>
-      {datas?.map((elem)=> (
-        <div key={elem.id} style={{border:"1px solid grey"}}>
-            <h2>Id: {elem.id}</h2>
-            <h3>Name: {elem.name}</h3>
-            <p>ph: {elem.ph}</p>
-            <p>tagline: {elem.tagline}</p>
-            <img src={elem.image_url} alt={elem.name} width={200} height={300}/>
+    <div style={{ margin: "5px", marginTop: "15px" }}>
+      {/* Display loading message and image when loading is true */}
+      {loading ? (
+        <div>
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrKxvWLyAmBbTKJMd_6x115Y9g5xpsX2fVsYhDMU2fBA&s"
+            alt="loading"
+          />
+          <h1>Loading...</h1>
         </div>
-      ))}
-
-      </div>
-      
+      ) : (
+        // Display data in a grid when loading is false
+        <div
+          style={{
+            display: "grid",
+            gridTemplateColumns: "repeat(4,1fr)",
+            gap: "5px",
+          }}
+        >
+          {" "}
+          {/* Map through datas array and display each card */}
+          {datas?.map((elem) => (
+            <div key={elem.id} style={{ border: "1px solid grey" }}>
+              <h2>Id: {elem.id}</h2>
+              <h3>Name: {elem.name}</h3>
+              <p>ph: {elem.ph}</p>
+              <p>Tagline: {elem.tagline}</p>
+              <p>contributed_by: {elem.contributed_by}</p>
+              <img
+                src={elem.image_url}
+                alt={elem.name}
+                width={200}
+                height={300}
+              />
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
