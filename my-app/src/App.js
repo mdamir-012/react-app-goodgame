80b90ff781101e00d3e4b9c3bc2573de14e9c40c
unknown <mdamir4298@gmail.com>
2023-12-03 00:07:57 +0530

    all done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,24 +8,36 @@ function App() {
   const [datas, setDatas] = useState([]);
   const [loading, setLoading] = useState(false);
   const [search, setSearch] = useState("");
+  const [currPage, setCurrPage] = useState(1);
 
+  const per_page = 8;
+  const total = Math.ceil(datas / per_page);
   // Function to construct the API URL with the search query
   const getData = (url, search) => {
     if (search) {
-      url = `${url}?beer_name=${search}`;
+      url = `${url}&beer_name=${search}`;
     }
     return url;
   };
 
   // Function to fetch data from the API
-  const fetchData = async () => {
-    let api = getData(`https://api.punkapi.com/v2/beers`, search);
+  const fetchData = async (currPage) => {
+    // Construct API URL with search and pagination parameters
+    let api = getData(
+      `https://api.punkapi.com/v2/beers?page=${currPage}&per_page=${per_page}`,
+      search
+    );
     try {
+      // Start loading data
       setLoading(true);
+      // Fetch data from the API
       let response = await fetch(api);
       let data = await response.json();
       // Log the fetched data to the console
       console.log(data);
+      const totalCount = Number(response.headers.get("X-Total-Count"));
+      console.log(totalCount);
+      // Set fetched data and stop loading
       setDatas(data);
       setLoading(false);
     } catch (err) {
@@ -38,8 +50,8 @@ function App() {
 
   // Effect hook to fetch data when the component mounts or when the search term changes
   useEffect(() => {
-    fetchData();
-  }, [search]);
+    fetchData(currPage);
+  }, [currPage, search]);
 
   // JSX structure for the App component
   return (
@@ -55,10 +67,26 @@ function App() {
           onChange={(e) => {
             setSearch(e.target.value);
             console.log(e.target.value);
+            setCurrPage(1);
           }}
         />
       </div>
       <Card datas={datas} loading={loading} />
+      <div>
+        <button
+          onClick={() => setCurrPage(currPage - 1)}
+          disabled={currPage === 1}
+        >
+          prev
+        </button>
+        {currPage}
+        <button
+          onClick={() => setCurrPage(currPage + 1)}
+          disabled={currPage === total}
+        >
+          next
+        </button>
+      </div>
     </div>
   );
 }
